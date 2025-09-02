d9239fc8c268449a80b695e836e0ede238b1133a
ralph <ralph.rebello8@gmail.com>
2025-09-02 20:43:54 -0300

    Primeiro commit

---
+++ calculos.py
@@ -0,0 +1,12 @@
+# Variaveis
+numero_1 = int(input("Informe o primeiro numero -> "))
+numero_2 = int(input("Informe o segundo numero -> "))
+nome = input("Informe o seu nome -> ")
+
+# soma = numero_1 + numero_2
+# print("A soma é -> ", soma)
+print("A soma é -> ", numero_1 + numero_2)
+print(nome, nome, nome)
+print("O resto da divisão é -> ", numero_1 % numero_2)
+print("A potencia de ", numero_1, " elevado em ", 
+      numero_2, "é -> ", numero_1 ** numero_2)
