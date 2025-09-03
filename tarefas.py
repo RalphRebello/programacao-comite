39ea66f02fa69bbf2b6f55c051f078a85d871fc6
ralph <ralph.rebello8@gmail.com>
2025-09-02 21:51:06 -0300

    criação do programa de tarefas

---
+++ tarefas.py
@@ -0,0 +1,19 @@
+# Calculando o dobro do numero informado
+numero_1 = int(input("Informe um numero inteiro -> "))
+print("O dobro do numero informado é -> ", numero_1 * 2)
+
+# Calculando a area do quadrado
+lado = float(input("Informe o valor do lado do quadrado -> "))
+print("A area do quadrado é -> ", lado * lado)
+
+# Calcular a idade em meses e dias
+idade = int(input("Informe sua idade -> "))
+meses = idade * 12
+dias = meses * 365
+print("Sua idade em meses é ", meses, "e em dias ", dias)
+
+nota1 = float(input("Informe a primeira nota -> "))
+nota2 = float(input("Informe a segunda nota -> "))
+nota3 = float(input("Informe a terceira nota -> "))
+
+print("A média é -> ", (nota1 + nota2 + nota3) / 3)
