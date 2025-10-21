00e4d536dc6c5eb65e06389da8b68358a14917c8
ralph <ralph.rebello8@gmail.com>
2025-10-21 19:42:00 -0300

    Update final dicionarios

--- dicionarios/contar_palavras.py
+++ dicionarios/contar_palavras.py
@@ -10,10 +10,14 @@ qtd_letras = {}
 
 for palavra in palavras:
 
-    chave = f'qtd_palavras - {len(palavra)}'
-    valor += 1
+    if f'qtd_palavras - {len(palavra)}' not in qtd_letras.keys():
+        chave = f'qtd_palavras - {len(palavra)}'
+        valor = 1
 
-    qtd_letras.update({chave: valor})
+        qtd_letras.update({chave: valor})
+    else:
+        qtd_letras[f'qtd_palavras - {len(palavra)}'] += 1
+
 
 for chave, valor in qtd_letras.items():
     print(f'{chave} | {valor}')
